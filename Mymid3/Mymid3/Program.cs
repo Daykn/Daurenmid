511a398bce8292e7db508d335c847f9c0c2ebed6
Daykn <dauer_01_10@mail.ru>
2017-03-09 12:08:19 +0600

    Add files via upload
    number3

---
+++ Mymid3/Mymid3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mymid3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(Console.ReadLine());
+            List<FileSystemInfo> li = new List<FileSystemInfo>();
+            li.AddRange(di.GetDirectories());
+            li.AddRange(di.GetFiles());
+            foreach (FileSystemInfo p in li) {
+                string str = p.Name;
+                char[] arr = str.ToCharArray();
+                for (int i = 0; i < p.Name.Length-3; i++) {
+                    if (arr[i] == 't' && arr[i+1] == 'e' && arr[i+2] == 's' && arr[i+3] == 't')
+                    {
+                        Console.WriteLine(p.Name);
+                    }
+                } 
+            }
+            Console.ReadKey();
+        }
+    }
+}
