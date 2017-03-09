67f435569e3432ee490f6a3ed1b869cdfe49acfc
Daykn <dauer_01_10@mail.ru>
2017-03-09 11:26:17 +0600

    Add files via upload
    number1

---
+++ Mymid1/Mymid1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mymid1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(str);
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    Console.Write(i);
+                    Console.Write(' ');
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
